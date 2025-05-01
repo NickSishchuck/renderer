9a287d30f6d0911a3b6cedb21f1a00c44cde9c23
NickSishchuck <mikiitasisuk@gmail.com>
2025-05-01 13:14:35 +0300

    A triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
+#include <cmath>
 #include <iostream>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+const char* vertexShaderSource = "#version 330 core \n"
+"layout (location = 0) in vec3 aPos; \n"
+"void main()\n"
+"{\n"
+"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\n\0";
+
+const char* fragmentShaderSource = "#version 330 core \n"
+"out vec4 FragColor; \n"
+"void main()\n"
+"{\n"
+"    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
 
 // Error callback for GLFW
 void errorCallback(int error, const char* description) {
@@ -14,6 +29,8 @@ int main() {
         return -1;
     }
 
+
+
     glfwSetErrorCallback(errorCallback);
 
     // Set OpenGL version and profile
@@ -21,8 +38,18 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    //Triangle
+    GLfloat vertices[] =
+    {
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
+        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+    };
+
+
+
     // Create a window
-    GLFWwindow* window = glfwCreateWindow(800, 600, "GLFW + GLEW Example", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Basic renderer", nullptr, nullptr);
     if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
@@ -50,18 +77,65 @@ int main() {
     glfwGetFramebufferSize(window, &width, &height);
     glViewport(0, 0, width, height);
 
+    //GLuint is an OpenGL version for unsigned int
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    //wrapping our shaders into a so called shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+
+    glLinkProgram(shaderProgram);
+
+    //for some reason a guy in the guide told me that the shaders are already in the program, and we can delete them
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+   //Creating a buffer to send data between cpu and gpu
+
+   GLuint VAO, VBO;
+   glGenVertexArrays(1, &VAO);
+   glGenBuffers(1, &VAO);
+
+   glBindVertexArray(VAO);
+
+   glGenBuffers(1, &VBO);
+  // Binding in OpenGL is the way to make an object a *current* object
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+ glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+ glVertexAttribPointer(0,3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+ glEnableVertexAttribArray(0);
     // Main loop
     while (!glfwWindowShouldClose(window)) {
         // Clear the screen
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
         // Swap buffers and poll events
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
+
     // Clean up
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteProgram(shaderProgram);
+
     glfwTerminate();
     return 0;
 }
