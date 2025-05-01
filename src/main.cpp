da0439b966c4b862896cf9f106a9c2b97ab31d03
NickSishchuck <mikiitasisuk@gmail.com>
2025-05-01 13:50:18 +0300

    Triangle inside other triangles

--- src/main.cpp
+++ src/main.cpp
@@ -41,9 +41,19 @@ int main() {
     //Triangle
     GLfloat vertices[] =
     {
-        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,
-        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f
+        -0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, //Lower left corner
+        0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, //Lower right corner
+        0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, //Upper corner
+        -0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, //Inner left
+        0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, //Inner right
+        0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+    };
+
+    GLuint indices[] = // indices are the thing that define the order of vertices to draw
+    {
+        0, 3, 5,
+        3, 2, 4,
+        5, 4, 1
     };
 
 
@@ -98,18 +108,24 @@ int main() {
     glDeleteShader(fragmentShader);
 
    //Creating a buffer to send data between cpu and gpu
+   // Vertex Array Object, Vertex Buffer Object, Element Buffer Object
+   GLuint VAO, VBO, EBO;
 
-   GLuint VAO, VBO;
+
    glGenVertexArrays(1, &VAO);
-   glGenBuffers(1, &VAO);
+   //creating a buffer object
+   //we only have 1 object to pass, so we type 1, then the address of the variable
+   glGenBuffers(1, &VBO);
+   glGenBuffers(1, &EBO);
 
    glBindVertexArray(VAO);
 
-   glGenBuffers(1, &VBO);
   // Binding in OpenGL is the way to make an object a *current* object
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+ glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+ glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 
  glVertexAttribPointer(0,3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -122,8 +138,7 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
+        glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 
         // Swap buffers and poll events
         glfwSwapBuffers(window);
@@ -134,6 +149,7 @@ int main() {
     // Clean up
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     glfwTerminate();
